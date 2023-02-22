4147f49ae689502ec73a93ca4f16461ae4cf4bae
Ngduong29 <cflagia123@gmail.com>
2023-02-22 14:30:06 +0700

    day database len

---
+++ database/btth01_cse485_ex.sql
@@ -0,0 +1,342 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.0
+-- https://www.phpmyadmin.net/
+--
+-- Máy chủ: 127.0.0.1:3307
+-- Thời gian đã tạo: Th2 22, 2023 lúc 06:07 AM
+-- Phiên bản máy phục vụ: 10.4.24-MariaDB
+-- Phiên bản PHP: 8.1.6
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Cơ sở dữ liệu: `btth01_cse485`
+--
+
+DELIMITER $$
+--
+-- Thủ tục
+--
+CREATE DEFINER=`` PROCEDURE `sp_DSBaiViet` (IN `tentheloai` VARCHAR(200))   BEGIN
+  DECLARE matheloai INT;
+
+  SELECT ma_tloai INTO matheloai FROM theloai 
+  WHERE ten_tloai = tentheloai;
+
+  IF matheloai IS NULL THEN
+    SELECT "Thể loại không tồn tại" AS message;
+  ELSE
+    SELECT * FROM baiviet WHERE matheloai = baiviet.ma_tloai;
+  END IF;
+END$$
+
+DELIMITER ;
+
+-- --------------------------------------------------------
+
+--
+-- Cấu trúc bảng cho bảng `baiviet`
+--
+
+CREATE TABLE `baiviet` (
+  `ma_bviet` int(10) UNSIGNED NOT NULL,
+  `tieude` varchar(200) NOT NULL,
+  `ten_bhat` varchar(100) NOT NULL,
+  `ma_tloai` int(10) UNSIGNED NOT NULL,
+  `tomtat` text NOT NULL,
+  `noidung` text DEFAULT NULL,
+  `ma_tgia` int(10) UNSIGNED NOT NULL,
+  `ngayviet` datetime NOT NULL DEFAULT current_timestamp(),
+  `hinhanh` varchar(200) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Đang đổ dữ liệu cho bảng `baiviet`
+--
+
+INSERT INTO `baiviet` (`ma_bviet`, `tieude`, `ten_bhat`, `ma_tloai`, `tomtat`, `noidung`, `ma_tgia`, `ngayviet`, `hinhanh`) VALUES
+(1, 'Lòng mẹ', 'Lòng mẹ', 2, 'Và mẹ ơi đừng khóc nhé! Cả đời này mẹ đã khóc nhiều lắm rồi, hãy cười lên vì con đã trưởng thành! Con sẽ lại về dậy sớm nấu cơm cho mẹ, nấu nước cho mẹ tắm như ngày xưa. “Dù cho vai nắng nhưng lòng thương chẳng nhạt màu, vẫn mơ quay về vui vầy dưới bóng mẹ yêu”', NULL, 1, '2012-07-23 00:00:00', NULL),
+(2, 'Cảm ơn em đã rời xa anh', 'Vết mưa', 2, 'Cảm ơn em đã cho anh những tháng ngày hạnh phúc, cho anh biết yêu và được yêu. Em cho anh được nếm trải hương vị ngọt ngào của tình yêu nhưng cũng đầy đau khổ và nước mắt. Những tháng ngày đó có lẽ suốt cuộc đời anh không bao giờ quên', NULL, 3, '2012-02-12 00:00:00', NULL),
+(3, 'Cuộc đời có mấy ngày mai?', 'Phôi pha', 2, 'Đêm nay, trời quang mây tạnh, trong người nghe hoang vắng và tôi ngồi đây “Ôm lòng đêm, Nhìn vầng trăng mới về” mà ngậm ngùi “Nhớ chân giang hồ. Ôi phù du, từng tuổi xuân đã già”', NULL, 4, '2014-03-13 00:00:00', NULL),
+(4, 'Quê tôi!', 'Quê hương', 5, 'Quê hương là gì mà chở đầy kí ức nhỏ xinh. Có đám trẻ nô đùa bên nhau dưới gốc ổi nhà bà Năm giữa trưa nắng gắt chỉ để chờ bà đi vắng là hái trộm. Có hai anh em tôi bì bõm lội sình bắt cua đem về nhà cho mẹ nấu canh, nấu cháo… Có ba chị em tôi lục đục tự nấu ăn khi mẹ vắng nhà. Có anh tôi luôn dắt tôi đi cùng đường ngõ xóm chỉ để em được vui. Có cả những trận cãi nhau nảy lửa của ba anh em nữa…', NULL, 5, '2014-02-20 00:00:00', NULL),
+(5, 'Đất nước', 'Đất nước', 5, 'Đã bao nhiêu lần tôi tự hỏi: liệu trên Thế giới này có nơi nào chiến tranh tang thương mà lại rất đổi anh hùng như nước mình không? Liệu có mảnh đất nào mà mỗi tấc đất hôm nay đã thấm máu xương của những thế hệ đi trước nhiều như nước mình không? Và, liệu có một đất nước nào lại có nhiều bà mẹ đau khổ nhưng cũng hết sức gan góc như đất nước mình không?', NULL, 1, '2010-05-25 00:00:00', NULL),
+(6, 'Hard Rock Hallelujah', 'Hard Rock Hallelujah', 7, 'Những linh hồn đang lạc lối, mù quáng mất phương hướng trong cõi trần gian đầy nghiệt ngã hãy nên lắng nghe \"Hard Rock Hallelujah\" để có thể quên tất cả mọi thứ để tìm về đúng bản chất sâu thẳm nhất trong tâm hồn chính mình!', NULL, 6, '2013-09-12 00:00:00', NULL),
+(7, 'The Unforgiven', 'The Unforgiven', 7, 'Lâu lắm rồi mới nghe lại The Unforgiven II, vì bài này không phải là bài mà tôi thích. Anh bạn tôi lúc trước, đi đâu cũng nghêu ngao bài này ấy, chỉ tại vì hắn đang... thất tình mà lị. Mà sao Metallica có The Unforgiven rồi lại có thêm bài này chi nữa vậy không biết nữa, làm cho tôi cảm thấy hình như hơi bị đúng so với tâm trạng của tôi lúc này.', NULL, 1, '2010-05-25 00:00:00', NULL),
+(8, 'Nơi tình yêu bắt đầu', 'Nơi tình yêu bắt đầu', 1, 'Nhiều người sẽ nghĩ làm gì có yêu nhất và làm gì có yêu mãi. Ừ! Chẳng có gì là mãi mãi cả, vì chúng ta không trường tồn vĩnh cửu', NULL, 1, '2014-02-03 00:00:00', NULL),
+(9, 'Love Me Like There’s No Tomorrow', 'Love Me Like There’s No Tomorrow', 8, 'Nếu ai đã từng yêu Queen, yêu cái chất giọng cao, sắc sảo như một vết cắt thật ngọt ẩn giấu bao cảm xúc mãnh liệt của Freddie chắc không thể không \"điêu đứng\" mỗi khi nghe Love Me Like There’s No Tomorrow.', NULL, 1, '2013-02-26 00:00:00', NULL),
+(10, 'I\'m stronger', 'I\'m stronger', 7, 'Em không phải là người giỏi giấu cảm xúc, nhưng em lại là người giỏi đoán biết cảm xúc của người khác vậy nên đừng cố nói nhớ em, rằng mọi thứ chỉ là do hoàn cảnh. Và cũng đừng dối em rằng anh đã từng yêu em. Em nhắm mắt cũng cảm nhận được mà, thật đấy', NULL, 2, '2013-08-21 00:00:00', NULL),
+(11, 'Ôi Cuộc Sống Mến Thương', 'Ôi Cuộc Sống Mến Thương', 5, 'Có một câu nói như thế này \"Âm nhạc là một cái gì khác lạ mà hầu như tôi muốn nói nó là một phép thần diệu.Vì nó đứng giữa tư tưởng và hiện tượng, tinh thần và vật chất, mọi thứ trung gian mơ hồ thế đó mà không là thế đó giữa các sự vật mà âm nhạc hòa giải\"', NULL, 2, '2011-10-09 00:00:00', NULL),
+(12, 'Cây và gió', 'Cây và gió', 7, 'Em và anh, hai đứa quen nhau thật tình cờ. Lời hát của anh từ bài hát “Cây và gió” đã làm tâm hồn em xao động. Nhưng sự thật phũ phàng rằng em chưa bao giờ nói cho anh biết những suy nghĩ tận sâu trong tim mình. Bởi vì em nhút nhát, em không dám đối mặt với thực tế khắc nghiệt, hay thực ra em không dám đối diện với chính mình.', NULL, 7, '2013-12-05 00:00:00', NULL),
+(13, 'Như một cách tạ ơn đời', 'Người thầy', 2, 'Ánh nắng cuối ngày rồi cũng sẽ tắt, dòng sông con đò rồi cũng sẽ rẽ sang một hướng khác. Nhưng việc trồng người luôn cảm thụ với chuyến đò ngang, cứ tần tảo đưa rồi lặng lẽ quay về đưa sang. Con đò năm xưa của Thầy nặng trĩu yêu thương, hy sinh thầm lặng.', NULL, 8, '2014-01-02 00:00:00', NULL);
+
+-- --------------------------------------------------------
+
+--
+-- Cấu trúc bảng cho bảng `tacgia`
+--
+
+CREATE TABLE `tacgia` (
+  `ma_tgia` int(10) UNSIGNED NOT NULL,
+  `ten_tgia` varchar(100) NOT NULL,
+  `hinh_tgia` varchar(100) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Đang đổ dữ liệu cho bảng `tacgia`
+--
+
+INSERT INTO `tacgia` (`ma_tgia`, `ten_tgia`, `hinh_tgia`) VALUES
+(1, 'Nhacvietplus', NULL),
+(2, 'Sưu tầm', NULL),
+(3, 'Sandy', NULL),
+(4, 'Lê Trung Ngân', NULL),
+(5, 'Khánh Ngọc', NULL),
+(6, 'Night Stalker', NULL),
+(7, 'Phạm Phương Anh', NULL),
+(8, 'Tâm tình', NULL);
+
+-- --------------------------------------------------------
+
+--
+-- Cấu trúc bảng cho bảng `theloai`
+--
+
+CREATE TABLE `theloai` (
+  `ma_tloai` int(10) UNSIGNED NOT NULL,
+  `ten_tloai` varchar(50) NOT NULL,
+  `SLBaiViet` int(11) NOT NULL DEFAULT 0
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Đang đổ dữ liệu cho bảng `theloai`
+--
+
+INSERT INTO `theloai` (`ma_tloai`, `ten_tloai`, `SLBaiViet`) VALUES
+(1, 'Nhạc trẻ', 1),
+(2, 'Nhạc trữ tình', 4),
+(3, 'Nhạc cách mạng', 0),
+(4, 'Nhạc thiếu nhi', 0),
+(5, 'Nhạc quê hương', 3),
+(6, 'POP', 0),
+(7, 'Rock', 4),
+(8, 'R&B', 1);
+
+-- --------------------------------------------------------
+
+--
+-- Cấu trúc bảng cho bảng `users`
+--
+
+CREATE TABLE `users` (
+  `id` int(11) NOT NULL,
+  `username` varchar(50) NOT NULL,
+  `password` varchar(255) NOT NULL,
+  `role` enum('user','admin') DEFAULT 'user'
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Đang đổ dữ liệu cho bảng `users`
+--
+
+INSERT INTO `users` (`id`, `username`, `password`, `role`) VALUES
+(1, 'admin', 'admin', 'admin'),
+(2, 'admin2', 'admin', 'admin');
+
+-- --------------------------------------------------------
+
+--
+-- Cấu trúc đóng vai cho view `vw_music`
+-- (See below for the actual view)
+--
+CREATE TABLE `vw_music` (
+`tieude` varchar(200)
+,`ten_bhat` varchar(100)
+,`ten_tloai` varchar(50)
+,`tomtat` text
+,`noidung` text
+,`ten_tgia` varchar(100)
+,`ngayviet` datetime
+,`hinhanh` varchar(200)
+);
+
+-- --------------------------------------------------------
+
+--
+-- Cấu trúc cho view `vw_music`
+--
+DROP TABLE IF EXISTS `vw_music`;
+
+CREATE ALGORITHM=UNDEFINED DEFINER=`` SQL SECURITY DEFINER VIEW `vw_music`  AS SELECT `baiviet`.`tieude` AS `tieude`, `baiviet`.`ten_bhat` AS `ten_bhat`, `theloai`.`ten_tloai` AS `ten_tloai`, `baiviet`.`tomtat` AS `tomtat`, `baiviet`.`noidung` AS `noidung`, `tacgia`.`ten_tgia` AS `ten_tgia`, `baiviet`.`ngayviet` AS `ngayviet`, `baiviet`.`hinhanh` AS `hinhanh` FROM ((`baiviet` join `tacgia` on(`baiviet`.`ma_tgia` = `tacgia`.`ma_tgia`)) join `theloai` on(`baiviet`.`ma_tloai` = `theloai`.`ma_tloai`))  ;
+
+--
+-- Chỉ mục cho các bảng đã đổ
+--
+
+--
+-- Chỉ mục cho bảng `baiviet`
+--
+ALTER TABLE `baiviet`
+  ADD PRIMARY KEY (`ma_bviet`),
+  ADD KEY `ma_tloai` (`ma_tloai`),
+  ADD KEY `ma_tgia` (`ma_tgia`);
+
+--
+-- Chỉ mục cho bảng `tacgia`
+--
+ALTER TABLE `tacgia`
+  ADD PRIMARY KEY (`ma_tgia`);
+
+--
+-- Chỉ mục cho bảng `theloai`
+--
+ALTER TABLE `theloai`
+  ADD PRIMARY KEY (`ma_tloai`);
+
+--
+-- Chỉ mục cho bảng `users`
+--
+ALTER TABLE `users`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `username` (`username`);
+
+--
+-- AUTO_INCREMENT cho các bảng đã đổ
+--
+
+--
+-- AUTO_INCREMENT cho bảng `baiviet`
+--
+ALTER TABLE `baiviet`
+  MODIFY `ma_bviet` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=14;
+
+--
+-- AUTO_INCREMENT cho bảng `tacgia`
+--
+ALTER TABLE `tacgia`
+  MODIFY `ma_tgia` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
+
+--
+-- AUTO_INCREMENT cho bảng `theloai`
+--
+ALTER TABLE `theloai`
+  MODIFY `ma_tloai` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
+
+--
+-- AUTO_INCREMENT cho bảng `users`
+--
+ALTER TABLE `users`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
+--
+-- Các ràng buộc cho các bảng đã đổ
+--
+
+--
+-- Các ràng buộc cho bảng `baiviet`
+--
+ALTER TABLE `baiviet`
+  ADD CONSTRAINT `baiviet_ibfk_1` FOREIGN KEY (`ma_tloai`) REFERENCES `theloai` (`ma_tloai`),
+  ADD CONSTRAINT `baiviet_ibfk_2` FOREIGN KEY (`ma_tgia`) REFERENCES `tacgia` (`ma_tgia`);
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+
+
+--4. Thực hiện các yêu cầu truy vấn sau:
+--a, Liệt kê các bài viết về các bài hát thuộc thể loại Nhạc trữ tình (2 đ)
+SELECT * from baiviet , theloai 
+where baiviet.ma_tloai = theloai.ma_tloai and theloai.ten_tloai = "Nhạc trữ tình"
+
+--b. Liệt kê các bài viết của tác giả “Nhacvietplus” (2 đ)
+SELECT * from baiviet, tacgia 
+where baiviet.ma_tgia = tacgia.ma_tgia and tacgia.ten_tgia = "Nhacvietplus"
+
+--c. Liệt kê các thể loại nhạc chưa có bài viết cảm nhận nào. (2 đ)
+SELECT theloai.ten_tloai from theloai
+where theloai.ten_tloai NOT IN (SELECT theloai.ten_tloai from theloai, baiviet where theloai.ma_tloai = baiviet.ma_tloai)
+
+--d. Liệt kê các bài viết với các thông tin sau: mã bài viết, tên bài viết, tên bài hát, tên tác giả, tên thể loại, ngày viết. (2 đ)
+SELECT baiviet.ma_bviet, baiviet.tieude, baiviet.ten_bhat, tacgia.ten_tgia, theloai.ten_tloai, baiviet.ngayviet
+from baiviet, tacgia, theloai
+where baiviet.ma_tloai = theloai.ma_tloai and baiviet.ma_tgia = tacgia.ma_tgia
+
+--e. Tìm thể loại có số bài viết nhiều nhất (2 đ)
+SELECT theloai.ten_tloai , COUNT(*) as sotheloaico
+FROM baiviet
+JOIN theloai on baiviet.ma_tloai = theloai.ma_tloai
+GROUP BY theloai.ten_tloai
+HAVING COUNT(*) >= all 
+(SELECT COUNT(*) as sotheloaico
+FROM baiviet
+JOIN theloai on baiviet.ma_tloai = theloai.ma_tloai
+GROUP BY theloai.ten_tloai)
+--//ORDER by sotheloaico DESC
+
+--f. Liệt kê 2 tác giả có số bài viết nhiều nhất (2 đ)
+SELECT tacgia.ten_tgia, COUNT(*) as sobaiviet FROM baiviet
+JOIN tacgia ON baiviet.ma_tgia = tacgia.ma_tgia
+GROUP BY tacgia.ten_tgia
+ORDER BY sobaiviet DESC
+LIMIT 2
+
+--g. Liệt kê các bài viết về các bài hát có tựa bài hát chứa 1 trong các từ “yêu”, “thương”, “anh”, “em” (2 đ)
+SELECT * FROM baiviet
+WHERE ten_bhat LIKE '%yêu%' OR ten_bhat LIKE '%thương%'  OR ten_bhat LIKE '%anh%'  OR ten_bhat LIKE '%em%'
+
+--h. Liệt kê các bài viết về các bài hát có tiêu đề bài viết hoặc tựa bài hát chứa 1 trong các từ 
+--“yêu”, “thương”, “anh”, “em” (2 đ)
+SELECT * FROM baiviet
+WHERE tieude LIKE '%yêu%' OR tieude LIKE '%thương%'  OR tieude LIKE '%anh%'  OR tieude LIKE '%em%' OR ten_bhat LIKE '%yêu%' OR ten_bhat LIKE '%thương%'  OR ten_bhat LIKE '%anh%'  OR ten_bhat LIKE '%em%'
+
+--i. Tạo 1 view có tên vw_Music để hiển thị thông tin về Danh sách các bài viết kèm theo Tên thể loại và tên tác giả (2 đ)
+CREATE VIEW vw_Music AS
+SELECT baiviet.tieude, baiviet.ten_bhat, theloai.ten_tloai, baiviet.tomtat, baiviet.noidung, tacgia.ten_tgia, baiviet.ngayviet, baiviet.hinhanh FROM baiviet
+INNER JOIN tacgia ON baiviet.ma_tgia = tacgia.ma_tgia
+INNER JOIN theloai ON baiviet.ma_tloai = theloai.ma_tloai
+
+--j. Tạo 1 thủ tục có tên sp_DSBaiViet với tham số truyền vào là Tên thể loại và trả về danh sách Bài viết của thể loại đó. Nếu thể loại không tồn tại thì hiển thị thông báo lỗi. (2 đ)
+CREATE PROCEDURE sp_DSBaiViet (IN tentheloai VARCHAR(255))
+BEGIN
+  DECLARE matheloai INT;
+
+  SELECT ma_tloai INTO matheloai FROM theloai 
+  WHERE ten_tloai = tentheloai;
+
+  IF matheloai IS NULL THEN
+    SELECT "Thể loại không tồn tại" AS message;
+  ELSE
+    SELECT * FROM baiviet WHERE matheloai = baiviet.ma_tloai;
+  END IF;
+END
+
+--k. Thêm mới cột SLBaiViet vào trong bảng theloai. Tạo 1 trigger có tên tg_CapNhatTheLoai để khi thêm/sửa/xóa bài viết thì số lượng bài viết trong bảng theloai được cập nhật theo. (2 đ)
+ -- - Thêm mới cột:
+ALTER TABLE theloai ADD COLUMN SLBaiViet INT NOT NULL DEFAULT 0;
+ -- - Cập nhật cột sltheloai:
+UPDATE theloai t 
+SET t.SLBaiViet = (
+  SELECT COUNT(*) from baiviet bv
+  WHERE bv.ma_tloai = t.ma_tloai
+);
+ -- - Tạo trigger:
+
+
+--l. Bổ sung thêm bảng Users để lưu thông tin Tài khoản đăng nhập và sử dụng cho chức năng Đăng nhập/Quản trị trang web. (5 đ)
+CREATE TABLE Users (
+  id INT(11) NOT NULL AUTO_INCREMENT,
+  username VARCHAR(50) NOT NULL,
+  password VARCHAR(255) NOT NULL,
+  role ENUM('user', 'admin') DEFAULT 'user',
+  PRIMARY KEY (id),
+  UNIQUE KEY username (username)
+);
