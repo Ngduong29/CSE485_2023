e7b2b217350d561a4a916c37590653d92dbe632b
imthuynee <nguyenthithuyy2001@gmail.com>
2023-02-23 00:55:36 +0700

    commit new by Thuy

--- admin/delete_category.php
+++ admin/delete_category.php
@@ -1,13 +1,12 @@
-
 <?php
    include '../config.php';
    if(isset($_GET['deleteid'])){
     $id = $_GET['deleteid'];
 
-    $sql = "delete from tacgia where ma_tgia = $id";
+    $sql = "delete from theloai where ma_tloai = $id";
     $result = mysqli_query($conn,$sql);
     if($result){
-        header("location:author.php");
+        header("location:category.php");
     }
    }
 ?>
---
+++ admin/process_update_category.php
@@ -0,0 +1,18 @@
+<?php
+include '../config.php';
+include("header.php");
+        $id = $_GET['ma_tloai'];
+        $ten_tloai = $_POST['ten_tloai'];
+        $SLBaiViet = $_POST['SLBaiViet'];
+        
+
+        $sql = "UPDATE theloai SET ten_tloai = '$ten_tloai', SLBaiViet= '$SLBaiViet' where ma_tloai = '$id'";
+        if (mysqli_query($conn, $sql) == true) {
+
+            header("location:category.php");
+        } else {
+            echo mysqli_error($conn);
+        }
+   
+include("footer.php");
+?>
